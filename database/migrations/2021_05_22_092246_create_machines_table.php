696f499c367f516e7cef5bd6163491b30c4072bf
Enomartins <enowebservices@gmail.com>
2021-05-22 10:49:51 +0100

    machine controller and migration added

---
+++ database/migrations/2021_05_22_092246_create_machines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMachinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('price', 5, 2);
+            $table->string('image');
+            $table->string('brand');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('machines');
+    }
+}
